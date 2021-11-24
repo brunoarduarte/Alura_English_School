00d65763e4b7c4875e84ea163b4fc8c89d20e0b0
Bruno Duarte <brunoarduarte@gmail.com>
2021-11-24 10:16:29 -0300

    Implements files model, seeders and migrations && create table

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/teste', (req, res) => {
   res.status(200).send({ message: 'Welcome to my API' });
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
 
 module.exports = app;
